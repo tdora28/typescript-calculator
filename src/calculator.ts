275fdc912bc3f018ddeaf3086d237ff10489991d
tdora28 <tokaidora88@gmail.com>
2024-02-15 09:06:14 +0200

    Correct operator input cleaning

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,51 +3,59 @@ const display = document.querySelector('#display') as HTMLInputElement;
 // Shows "0" on the display initially
 display.value = '0';
 // Follows the different input states
-let activeState: 'init' | 'number' | 'operator' | 'result' = 'init';
+let activeState: 'initState' | 'numberState' | 'operatorState' | 'resultState' = 'initState';
 // Counts how many operators are in a row
 // E.g. "", "+", "*-" are acceptable
 let operatorCount: 0 | 1 | 2 = 0;
 
 function clearDisplay(): void {
   display.value = '0';
-  activeState = 'init';
+  activeState = 'initState';
   operatorCount = 0;
 }
 
 function inputNumber(num: number): void {
-  if ((activeState === 'init' || activeState === 'result') && num !== 0) {
+  if ((activeState === 'initState' || activeState === 'resultState') && num !== 0) {
     display.value = num.toString();
-    activeState = 'number';
-  } else if (activeState === 'operator' && num !== 0) {
+    activeState = 'numberState';
+  } else if (activeState === 'operatorState' && num !== 0) {
+    // Can't start with 0 after operator, because we don't have decimals
     display.value += num;
-    activeState = 'number';
-  } else if (activeState === 'number') {
+    activeState = 'numberState';
+  } else if (activeState === 'numberState') {
     display.value += num;
   }
 }
 
 function inputOperator(operator: string): void {
-  if (activeState === 'init' || activeState === 'number' || activeState === 'result') {
+  if (activeState === 'initState' || activeState === 'numberState' || activeState === 'resultState') {
     display.value += operator;
-    activeState = 'operator';
+    activeState = 'operatorState';
     operatorCount = 1;
-  } else if (activeState === 'operator' && operatorCount === 1) {
+  } else if (activeState === 'operatorState' && operatorCount === 1) {
     if (operator === '-' && display.value.slice(-1) !== '-') {
+      // This allows "+-", "*-", and "/-"
       display.value += operator;
       operatorCount = 2;
     } else {
+      // This switches the last entered operator to the new one
       display.value = display.value.slice(0, -1) + operator;
     }
+  } else if (activeState === 'operatorState' && operatorCount === 2) {
+    display.value = display.value.slice(0, -2) + operator;
+    operatorCount = 1;
   }
 }
 
 function calculateResult(): void {
-  if (activeState === 'operator') {
-    display.value = display.value.slice(0, -1);
+  if (activeState === 'operatorState') {
+    // In case there are trailing operators when hitting the equal sign
+    const cutAmount = -1 * operatorCount;
+    display.value = display.value.slice(0, cutAmount);
   }
   const result: number = eval(display.value);
   display.value = result.toString();
-  activeState = 'result';
+  activeState = 'resultState';
 }
 
 // Expose the functions to the global scope so they can be accessed from HTML
