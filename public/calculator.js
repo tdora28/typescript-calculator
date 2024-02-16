5974e4141e4f20f02ebef270532825e4f06f2318
tdora28 <tokaidora88@gmail.com>
2024-02-16 15:45:47 +0200

    Set tsconfig to remove comments from compiled file

--- public/calculator.js
+++ public/calculator.js
@@ -1,13 +1,8 @@
 "use strict";
 const display = document.querySelector('#display');
-// Shows "0" on the display initially
 display.value = '0';
-// Follows the different input states
 let activeState = 'initState';
-// Counts how many operators are in a row
-// E.g. "", "+", "*-" are acceptable
 let operatorCount = 0;
-// Helper to keep track of leading 0
 let zeroInFront = false;
 function clearDisplay() {
     display.value = '0';
@@ -56,12 +51,10 @@ function inputOperator(operator) {
     }
     else if (activeState === 'operatorState' && operatorCount === 1) {
         if (operator === '-' && display.value.slice(-1) !== '-') {
-            // This allows "+-", "*-", and "/-"
             display.value += operator;
             operatorCount = 2;
         }
         else {
-            // This switches the last entered operator to the new one
             display.value = display.value.slice(0, -1) + operator;
         }
     }
@@ -72,7 +65,6 @@ function inputOperator(operator) {
 }
 function calculateResult() {
     if (activeState === 'operatorState') {
-        // In case there are trailing operators when hitting the equal sign
         const cutAmount = -1 * operatorCount;
         display.value = display.value.slice(0, cutAmount);
     }
@@ -81,7 +73,6 @@ function calculateResult() {
     activeState = 'resultState';
     zeroInFront = false;
 }
-// Keyboard events
 document.addEventListener('keydown', (event) => {
     const { key } = event;
     if (!isNaN(Number(key))) {
@@ -98,12 +89,10 @@ document.addEventListener('keydown', (event) => {
     }
     event.preventDefault();
 });
-// Expose the functions to the global scope so they can be accessed from HTML
 const exposedFunctions = {
     clearDisplay,
     inputNumber,
     inputOperator,
     calculateResult,
 };
-// Attach the object to the window, under a single namespace
 window.Calc = exposedFunctions;
